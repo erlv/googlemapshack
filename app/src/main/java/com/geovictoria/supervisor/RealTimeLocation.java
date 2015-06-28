69ccfc9b2842580bc73e9d09e1fe48f3f7da57ed
Tristan Jones <tristanjones@berkeley.edu>
2015-06-27 20:40:57 -0700

    Refactored Android activity into android fragement

--- app/src/main/java/com/geovictoria/supervisor/RealTimeLocation.java
+++ app/src/main/java/com/geovictoria/supervisor/RealTimeLocation.java
@@ -25,8 +25,12 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -38,25 +42,55 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-public class RealTimeLocation extends Activity {
+public class RealTimeLocation extends Fragment {
 
     private GoogleMap map;
     private EditText location;
     private ArrayList<EmployeeCurloc> employees;
     LinearLayout ll;
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
 
-    //static final LatLng HAMBURG = new LatLng(53.558, 9.927);
-    //ArrayList<LatLng> list = new ArrayList<LatLng>();
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public static RealTimeLocation newInstance(String param1, String param2) {
+        RealTimeLocation fragment = new RealTimeLocation();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public RealTimeLocation() {
+        // Required empty public constructor
+    }
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_current_location);
-        ll = (LinearLayout) (findViewById(R.id.l1));
+
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =  inflater.inflate(R.layout.fragment_current_location, container, false);
+
+        ll = (LinearLayout) (v.findViewById(R.id.l1));
         addTest();
 
-        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+        map = ((MapFragment) this.getActivity().getFragmentManager().findFragmentById(R.id.map))
                 .getMap();
-        location = (EditText) findViewById(R.id.location);
+        location = (EditText) v.findViewById(R.id.location);
         for (EmployeeCurloc employee : employees) {
             if (employee.loc != null) {
                 map.addMarker(new MarkerOptions().position(new LatLng(
@@ -64,7 +98,11 @@ public class RealTimeLocation extends Activity {
                         .title(employee.userName));
             }
         }
+
+        return v;
     }
+
+
 
     private void addTest() {
         // TODO Auto-generated method stub
@@ -98,14 +136,14 @@ public class RealTimeLocation extends Activity {
 
     public void showInfo(ArrayList<Distance> dist) {
         for (Distance d : dist) {
-            TextView txt = new TextView(this);
+            TextView txt = new TextView(this.getActivity().getApplicationContext());
             txt.setText(d.employee.userName + "time :" + d.time2 +
                     "distance :" + d.distance2);
             ll.addView(txt);
         }
         for (EmployeeCurloc employee : employees) {
             if (employee.loc == null) {
-                TextView txt = new TextView(this);
+                TextView txt = new TextView(this.getActivity().getApplicationContext());
                 txt.setText(employee.userName + "is free");
                 ll.addView(txt);
             }
@@ -140,7 +178,7 @@ public class RealTimeLocation extends Activity {
 
     private void destError() {
         // TODO Auto-generated method stub
-        Context context = getApplicationContext();
+        Context context = this.getActivity().getApplicationContext();
         CharSequence text = "Sorry your location could not be found please try again!";
         int duration = Toast.LENGTH_LONG;
 
@@ -148,44 +186,10 @@ public class RealTimeLocation extends Activity {
         toast.show();
     }
 
-    /*public void click (View view){
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 12.0f));
-        Thread t = new Thread(new Runnable(){
 
-            @Override
-            public void run() {
-                // TODO Auto-generated method stub
-                try{
-                    HttpClient client = new DefaultHttpClient();
-                    double lat1 = HAMBURG.latitude;
-                    double long1 = HAMBURG.longitude;
-                    double lat2 = 52.5167;
-                    double long2 = 13.3833;
-                    String url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" +
-                            "37.386051,-122.083855&destinations=Sunnyvale&mode=driving&key=AIza" +
-                            "SyCVexmd9R_RhrcIwv9Z-nZYRxH5ZnZEq6c";
-                    HttpGet request = new HttpGet(url);
-                    Log.d("Reached Here", "HAHA");
-                    HttpResponse response = client.execute(request);
-                    Log.d("Response of GET request", response.toString());
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
-                    StringBuilder builder = new StringBuilder();
-                    for (String line = null; (line = reader.readLine()) != null;) {
-                        builder.append(line).append("\n");
-                    }
-                    JSONTokener tokener = new JSONTokener(builder.toString());
-                    JSONArray finalResult = new JSONArray(tokener);
-                }catch (Exception e){
-
-                }
-            }
-
-        });
-        t.start();
-    }*/
     public LatLng getLocationFromAddress(String strAddress) {
 
-        Geocoder coder = new Geocoder(this);
+        Geocoder coder = new Geocoder(this.getActivity().getApplicationContext());
         List<Address> address;
         try {
             address = coder.getFromLocationName(strAddress, 5);
